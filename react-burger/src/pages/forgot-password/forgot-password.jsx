a303fdf621ba075d4207b629a90fdc7a6a1b06ed
olesya-ovsya <54782641+olesya-ovsya@users.noreply.github.com>
2024-04-28 00:06:51 +0300

    sprint-3/step-1 запрос на проверку email при восстановлении пароля

--- react-burger/src/pages/forgot-password/forgot-password.jsx
+++ react-burger/src/pages/forgot-password/forgot-password.jsx
@@ -1,18 +1,53 @@
+import React from "react";
 import { 
     EmailInput,
     Button
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import '../../index.css';
 import { Link } from 'react-router-dom';
+import { sendRequest } from "../../utils/request-helper";
+import { useNavigate } from "react-router-dom";
 
 export default function ForgotPasswordPage() {
+
+    const navigate = useNavigate();
+
+    const [email, setEmail] = React.useState('');
+
+    const onClick = e => {
+
+        e.preventDefault(); // не даем странице перезагрузиться
+
+        const requestInfo = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({ email })
+        };
+
+        sendRequest("password-reset", requestInfo)
+        .then((model) => {
+            if (model && model.success) {
+                navigate('/reset-password', { replace: true });
+            } else {
+              throw new Error('Failed to receive data from the server. In the response model "success":false');
+            }
+          })
+        .catch(e => {});
+    };
+
+    const onChangeEmail = e => {
+        setEmail(e.target.value);
+    };
+
     return (
         <div className='form-container'>
               <form className='mt-20'>
                   <h1 className='text_type_main-medium mb-6'>Восстановление пароля</h1>
                   <div>
-                      <EmailInput extraClass='mb-6' placeholder='Укажите e-mail' />
-                      <Button size='large' extraClass='mb-20 pr-15 pl-15'>Восстановить</Button>
+                      <EmailInput value={email} onChange={onChangeEmail} extraClass='mb-6' placeholder='Укажите e-mail' />
+                      <Button  htmlType='submit' size='large' onClick={onClick} extraClass='mb-20 pr-15 pl-15'>Восстановить</Button>
                   </div>
                   <p className='text_type_main-default text_color_inactive'>
                     Вспомнили пароль?  <Link className='link-text' to='/login'>Войти</Link>
